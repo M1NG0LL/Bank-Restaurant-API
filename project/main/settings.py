bc5da83d49a973392c57c81b6c6d6439e3b67940
ᴍɪɴɢᴏʟʟ <151789265+M1NG0LL@users.noreply.github.com>
2024-08-25 18:03:51 +0300

    Pushing the code final form

--- project/main/settings.py
+++ project/main/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fatahmed1928@gmail.com' 
-EMAIL_HOST_PASSWORD = 'dfbm kfdn ulll hela'
+EMAIL_HOST_USER = 'Youremail@gmail.com' 
+EMAIL_HOST_PASSWORD = 'your app password'
